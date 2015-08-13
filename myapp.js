c73175d90e07c0177380bcc4353475a07c8f22b1
junkyuser <junky@no-spam.ws>
2015-08-13 23:12:26 +0100

    Update and rename myapp.js to books.json

--- myapp.js
+++
@@ -1,8 +0,0 @@
-var page = tabris.create("Page", {
-  topLevel: true,
-  title: "myapp"
-});
-tabris.create("TextView", {
-  layoutData: {centerX: 0, centerY: 0},
-  text: "My First App"
-}).appendTo(page);
